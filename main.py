b35505889d30f0cba45cb65c5de11f867b120094
bambriz <beambriz2017@gmail.com>
2021-12-05 05:46:26 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+#All this stuff makes it so we can use in repl
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://youtube.com")
+
+#The website we will do tests on
+driver.get("https://joindiaspora.com/")
 
