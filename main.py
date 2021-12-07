f49c811501813ea4be84ad263dc4b993a410bd49
bambriz <beambriz2017@gmail.com>
2021-12-07 03:44:29 +0000

    Added loggin test. Works super well!

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=chrome_options)
 
 userNTextID = "user_username"
+userPTextID = "user_password"
+commitButtonName = "commit"
+profAdd = "https://podington.oksocial.net/u/dummy123"
 
 # #All this stuff makes it so we can use in repl
 # chrome_options = Options()
@@ -32,7 +35,7 @@ class DiasporaTest(unittest.TestCase):
 	def test_1(self):
 		#All this stuff makes it so we can use in repl
 		
-		print("test 1")
+		print("test 1 to Verify we are on the right website")
 		
 		#wait for webpage to load
 		time.sleep(2)
@@ -41,17 +44,37 @@ class DiasporaTest(unittest.TestCase):
 	
 	def test_2(self):
 		time.sleep(1)
-		print("Second Test")
+		print("Second Test Loggin to dummy account")
 		time.sleep(1)
 		print("It is good")
 		usernameBox = driver.find_element(By.ID,userNTextID)
 		usernameBox.clear()
 		usernameBox.send_keys("dummy123")
+		time.sleep(1)
+		passwordBox = driver.find_element(By.ID,userPTextID)
+		passwordBox.clear()
+		passwordBox.send_keys("12345@@@@@")
+		time.sleep(1)
+		print("Username and Password have been typed")
+		submitButton = driver.find_element(By.NAME, commitButtonName)
+		submitButton.click()
 		time.sleep(2)
-
-		assert "dummy123" in usernameBox.get_attribute('value')
+		print("Logged in. Now Verify Correct Account.")
+		driver.get(profAdd)
+		time.sleep(2)
+		print("Verifying Account...")
+		avatar = driver.find_element(By.CLASS_NAME, "avatar")
+		correctAccount = "dummy123" in avatar.get_attribute('alt')
+		time.sleep(3)
+		print("Going back to home page...")
+		driver.back()
+		time.sleep(1)
+		if correctAccount:
+			print("Account verified!")
+		assert correctAccount
 
 	def test_3(self):
+		#Change this to be the last TestCase whenerver you make one
 		print("Closing Tab.")
 		driver.close()
 		assert True
